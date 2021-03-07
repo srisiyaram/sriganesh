8ff6296261845c032d8a9257a0441cbc1fa92b48
srisiyaram <jaysrisitaram@outlook.com>
2021-03-08 01:20:55 +0530

    'start'

---
+++ sample-cicd/src/main/java/com/example/demo/App.java
@@ -0,0 +1,15 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@SpringBootApplication
+public class App {
+	public static void main(String[] args) {
+		log.info("SHREE GANESH PRASANNA 579108");
+		SpringApplication.run(App.class, args);
+	}
+}
